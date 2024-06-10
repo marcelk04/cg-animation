60db557599ebd3bef02ce3a126ba3f9256f1e4e2
DefensivLord <marcel.kaempchen@gmail.com>
2024-06-10 23:24:29 +0200

    Setup basic rendering pipeline.

--- src/mainapp.cpp
+++ src/mainapp.cpp
@@ -8,7 +8,52 @@ using namespace glm;
 
 #include "framework/app.hpp"
 
+#include "framework/imguiutil.hpp"
+
 MainApp::MainApp() : App(800, 600) {
     App::setTitle("cgintro"); // set title
     App::setVSync(true); // Limit framerate
+
+    mesh.load("meshes/bunny.obj");
+    meshshader.load("meshshader.vert", "meshshader.frag");
+
+    lightDir = glm::vec3(1.0f);
+}
+
+void MainApp::init() {
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+}
+
+void MainApp::buildImGui() {
+    ImGui::StatisticsWindow(delta, resolution);
+
+    if (ImGui::SphericalSlider("Light Direction", lightDir)) {
+        meshshader.set("uLightDir", lightDir);
+    }
+}
+
+void MainApp::render() {
+    if (boringCamera.updateIfChanged()) {
+        meshshader.set("uWorldToClip", boringCamera.projectionMatrix * boringCamera.viewMatrix);
+    }
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    meshshader.bind();
+    mesh.draw();
+}
+
+void MainApp::keyCallback(Key key, Action action) {
+
+}
+
+void MainApp::scrollCallback(float amount) {
+    boringCamera.zoom(amount);
+}
+
+void MainApp::moveCallback(const vec2& movement, bool leftButton, bool rightButton, bool middleButton) {
+    if (leftButton || rightButton || middleButton) {
+        boringCamera.rotate(0.01f * movement);
+    }
 }
--- src/mainapp.hpp
+++ src/mainapp.hpp
@@ -5,20 +5,28 @@ using namespace glm;
 
 #include "framework/app.hpp"
 
+#include "framework/mesh.hpp"
+#include "framework/camera.hpp"
+#include "framework/gl/program.hpp"
+
 class MainApp : public App {
    public:
     MainApp();
 
    protected:
-    // void init() override;
-    // void buildImGui() override;
-    // void render() override;
-    // void keyCallback(Key key, Action action) override;
+    void init() override;
+    void buildImGui() override;
+    void render() override;
+    void keyCallback(Key key, Action action) override;
     // void clickCallback(Button button, Action action, Modifier modifier) override;
-    // void scrollCallback(float amount) override;
-    // void moveCallback(const vec2& movement, bool leftButton, bool rightButton, bool middleButton) override;
+    void scrollCallback(float amount) override;
+    void moveCallback(const vec2& movement, bool leftButton, bool rightButton, bool middleButton) override;
     // void resizeCallback(const vec2& resolution) override;
 
    private:
-    // ....
+       Mesh mesh;
+       Program meshshader;
+       Camera boringCamera;
+
+       glm::vec3 lightDir;
 };
