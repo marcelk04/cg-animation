dae11c84d9e870b3ebbba703f7d113ef9cbc16a6
timnogga <tim@nogga.eu>
2024-07-11 18:35:44 +0200

    zeit für nen bissel debugging

---
+++ src/animator.cpp
@@ -0,0 +1,3 @@
+//
+// Created by timnogga on 7/11/24.
+//
---
+++ src/animator.hpp
@@ -0,0 +1,8 @@
+//
+// Created by timnogga on 7/11/24.
+//
+
+#ifndef CG_ANIMATION_ANIMATOR_HPP
+#define CG_ANIMATION_ANIMATOR_HPP
+
+#endif //CG_ANIMATION_ANIMATOR_HPP
